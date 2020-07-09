903c332877230a2a2ec79dcd0d5273309cc14a59
ThangBT <thangbt1307@gmail.com>
2020-07-09 17:47:27 +0700

    update view

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -120,8 +120,8 @@
                               @if($prev)
                                     <a href="{{ route('post.single', ['slug' => $prev->slug ]) }}" class="btn-next-wrap">
                                           <div class="btn-content">
-                                          <div class="btn-content-title">Next Post</div>
-                                          <p class="btn-content-subtitle">{{ $prev->title }}</p>
+                                          <div class="btn-content-title">Tiếp</div>
+{{--                                          <p class="btn-content-subtitle">{{ $prev->title }}</p>--}}
                                           </div>
                                           <svg class="btn-next">
                                           <use xlink:href="#arrow-right"></use>
@@ -135,8 +135,8 @@
                                           <use xlink:href="#arrow-left"></use>
                                           </svg>
                                           <div class="btn-content">
-                                          <div class="btn-content-title">Previous Post</div>
-                                          <p class="btn-content-subtitle">{{ $next->title }}</p>
+                                          <div class="btn-content-title">Trở lại</div>
+{{--                                          <p class="btn-content-subtitle">{{ $next->title }}</p>--}}
                                           </div>
                                     </a>
                               @endif
